bd68269f900bae0abcbd1ed0b5e55ca8d1ad0c86
Thiago Martins <devthiagomartins@gmail.com>
2022-01-18 14:17:00 -0300

    Find Category byId and Exception Handling

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -2,14 +2,13 @@ package com.devsuperior.dscatalog.services;
 
 import java.util.List;
 import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -23,6 +22,14 @@ public class CategoryService {
 		List<Category> list = repository.findAll();
 
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		
+		Category entity = repository.findById(id).orElseThrow(()-> new EntityNotFoundException("Entity not found"));
+		
+		return new CategoryDTO(entity);
 	}
 
 }
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,3 +1,3 @@
 INSERT INTO tb_category (name) values ('Books');
 INSERT INTO tb_category (name) values ('Electronics');
-INSERT INTO tb_category (name) values ('Computes');
+INSERT INTO tb_category (name) values ('Computers');
